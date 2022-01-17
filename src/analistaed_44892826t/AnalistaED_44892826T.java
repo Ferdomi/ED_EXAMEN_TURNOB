8b8ad4a548981fbb9007882ee7d883e4c3167a2d
FERNANDO DOMINGUEZ <ferixet@mail.com>
2022-01-17 16:15:34 +0100

    versión final con javadoc DNI 44892826T

--- src/analistaed_44892826t/AnalistaED_44892826T.java
+++ src/analistaed_44892826t/AnalistaED_44892826T.java
@@ -1,8 +1,8 @@
 package analistaed_44892826t;
 
 /**
- * @title EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
- * @author FERNANDO DOMÍNGUEZ DNI 44892826T
+ * @title EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED 
+ * @author FERNANDO DOMÍNGUEZ DNI 44892826T 
  * @version 1.0
  */
 import java.util.*;
@@ -12,9 +12,9 @@ public class AnalistaED_44892826T {
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
  /**
-  * Constructor con tres parámetros
-  * @param nombreCompleto nombre completo en mayúsculas
-  * @param dni documento nacional de identidad con letra y sin espacios
+  * Constructor con tres  parámetros
+  * @param nombreCompleto nombre completo en mayúsculas 
+  * @param dni documento nacional de identidad con letra y sin espacio
   */
     public AnalistaED_44892826T(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
@@ -22,10 +22,10 @@ public class AnalistaED_44892826T {
     }
     
 /**
- * Calcula el salario bruto mensual en función del salario base por hora y las horas extras.
- * El precio por hora de la extra se determina con la constante FACTOR_HORA_EXTRA
- * @param horasExtra número de horas extra dedicadas redondeando a la baja (entero)
- * @return devuelve el salario bruto mensual en euros con dos decimales
+ * Calcula el salario bruto mensual en funcion del salario base por hora y las horas extras.
+ * El precio por hora de la extra se determina con la constante  FACTOR_HORA_EXTRA.
+ * @param horasExtra número de horas extra dedicadas redondeando a la baja (entero).
+ * @return devuelve el salario bruto mensual en euros con dos decimales.
  */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
